51eaf59147d704c46f40bbc30c6f87880e341274
dunglq3110 <144051000+dunglq3110@users.noreply.github.com>
2024-10-25 23:26:37 +0700

    asd

--- LaserTag.Host/Helper/Converters.cs
+++ LaserTag.Host/Helper/Converters.cs
@@ -6,17 +6,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows;
+using LaserTag.Host.Models;
+using System.Windows.Media;
 
 namespace LaserTag.Host.Helper
 {
-    public class EnumToVisibilityConverter : IValueConverter
+    public class HitTypeToBackgroundConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null || parameter == null)
-                return Visibility.Collapsed;
+            if (value is HitType hitType)
+            {
+                return hitType switch
+                {
+                    HitType.Normal => Color.FromRgb(128, 128, 128),    // Gray
+                    HitType.SSketch => Color.FromRgb(70, 130, 169),    // Blue
+                    HitType.Healing => Color.FromRgb(133, 159, 61),    // Green
+                    _ => Colors.Gray
+                };
+            }
+            return Colors.Gray;
+        }
 
-            return value.Equals(parameter) ? Visibility.Visible : Visibility.Collapsed;
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HitTypeToForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is HitType hitType)
+            {
+                return hitType switch
+                {
+                    HitType.Normal => Colors.White,
+                    HitType.SSketch => Colors.Black,
+                    HitType.Healing => Colors.Black,
+                    _ => Colors.White
+                };
+            }
+            return Colors.White;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
